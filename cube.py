1ae58b3d09070ebfc95e8d60d4807daf195d8404
MasterAcnolo <AxelTheo20072017@hotmail.com>
2024-05-27 21:01:33 +0200

    Create cube.py

---
+++ cube.py
@@ -0,0 +1,16 @@
+def cube(nombre):
+   
+    return nombre ** 3 # <- Change this value if you want to change exponent
+
+def main():
+   
+        nombre = float(input("Entrez un nombre : "))
+      
+        resultat = cube(nombre)
+        
+        print(f"Le cube de {nombre} est : {resultat}")
+    except ValueError:
+        print("Veuillez entrer un nombre valide.")
+
+if __name__ == "__main__":
+    main()
